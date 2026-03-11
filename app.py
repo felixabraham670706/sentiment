77239315c10896cbad32152c57c0c751fe786828
felixabraham670706 <felixabraham670706@gmail.com>
2026-03-12 00:32:37 +0400

    fix dotenv import

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
-from pipeline import run_pipeline
+import pandas as pd
 from datetime import datetime
 from streamlit_autorefresh import st_autorefresh
 import pytz
 
 st.title("Emirates NBD Reddit Sentiment Dashboard")
-# Dubai timezone
+
 dubai = pytz.timezone("Asia/Dubai")
 dubai_time = datetime.now(dubai)
 
-st.write("Last refreshed at:", dubai_time.strftime("%Y-%m-%d %H:%M:%S"), "Dubai Time")
+st.write("Last refreshed:", dubai_time.strftime("%Y-%m-%d %H:%M:%S"))
 
-# refresh every 5 minutes
 st_autorefresh(interval=300000)
 
-df = run_pipeline()
+df = pd.read_csv("sentiment.csv")
 
 positive = df[df["sentiment"]=="Positive"]
 neutral = df[df["sentiment"]=="Neutral"]
@@ -23,16 +22,16 @@ negative = df[df["sentiment"]=="Negative"]
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.subheader("Positive Comments")
+    st.subheader("Positive")
     for c in positive["comment"]:
         st.success(c)
 
 with col2:
-    st.subheader("Neutral Comments")
+    st.subheader("Neutral")
     for c in neutral["comment"]:
         st.warning(c)
 
 with col3:
-    st.subheader("Negative Comments")
+    st.subheader("Negative")
     for c in negative["comment"]:
         st.error(c)
